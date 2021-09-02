e4a0a3706fd604829aa6fe7410b0b25d3e5ad721
Morten Skogly <morten.skogly@gmail.com>
2021-09-02 19:48:09 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
+        bitbot.go(BBDirection.Forward, 60)
         basic.showIcon(IconNames.Happy)
     } else if (receivedNumber == 2) {
+        bitbot.stop(BBStopMode.Coast)
         basic.showIcon(IconNames.Asleep)
     } else if (receivedNumber == 3) {
+        bitbot.rotate(BBRobotDirection.Left, 60)
         basic.showLeds(`
             . . . . .
             . . . # .
@@ -12,6 +15,7 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
     } else if (receivedNumber == 4) {
+        bitbot.rotate(BBRobotDirection.Right, 60)
         basic.showLeds(`
             . . . . .
             . # . . .
