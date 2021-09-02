ae1b78789e2f403777c519bec9e4d0c32dab10ec
Morten Skogly <morten.skogly@gmail.com>
2021-09-02 19:43:10 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-    	
-    } else if (receivedNumber == 0) {
-    	
-    } else if (false) {
-    	
-    } else {
-    	
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
 })
 basic.showIcon(IconNames.Happy)
