4f040e5644a34c85d4140362cc0f0cd2d6951c3e
Morten Skogly <morten.skogly@gmail.com>
2021-09-02 19:34:29 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+basic.showIcon(IconNames.Happy)
+radio.setGroup(1)
+bitbot.select_model(BBModel.XL)
+basic.forever(function () {
+	
+})
